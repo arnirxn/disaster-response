58c75f7baf8a505884e7e70428ad1c0547dcd649
arnixn <arnfried.rixen@gmail.com>
2022-09-28 19:18:52 +0200

    Add functions to load, clean and write data

--- data/process_data.py
+++ data/process_data.py
@@ -3,45 +3,84 @@
 import pandas as pd
 from sqlalchemy import create_engine
 
-# load messages dataset
-messages = pd.read_csv("messages.csv")
 
-# load categories dataset
-categories = pd.read_csv("categories.csv")
+def load_data(messages_filepath, categories_filepath):
+    """."""
+    # load messages dataset
+    messages = pd.read_csv("messages.csv")
 
-# remove duplicates
-messages.drop_duplicates(inplace=True)
-categories.drop_duplicates(inplace=True)
+    # load categories dataset
+    categories = pd.read_csv("categories.csv")
 
-# merge datasets
-df = pd.merge(messages, categories, on="id")
+    # merge datasets
+    df = pd.merge(messages, categories, on="id")
 
-# create a dataframe of the 36 individual category columns
-categories = categories["categories"].str.split(";", expand=True)
+    return df
 
-# select the first row of the categories dataframe
-row = categories.iloc[0]
 
-# use this row to extract a list of new column names for categories.
-category_colnames = row.apply(lambda x: x[:-2])
+def clean_data(df):
+    """."""
+    # create a dataframe of the 36 individual category columns
+    categories = categories["categories"].str.split(";", expand=True)
 
-# rename the columns of `categories`
-categories.columns = category_colnames
+    # select the first row of the categories dataframe
+    row = categories.iloc[0]
 
-# convert category values to just numbers 0 or 1
-for column in categories:
-    categories[column] = categories[column].str[-1]
-    categories[column] = categories[column].astype(int)
+    # use this row to extract a list of new column names for categories.
+    category_colnames = row.apply(lambda x: x[:-2])
 
-# drop the original categories column from `df`
-df.drop("categories", axis=1, inplace=True)
+    # rename the columns of `categories`
+    categories.columns = category_colnames
 
-# concatenate the original dataframe with the new `categories` dataframe
-df = pd.concat([df, categories], axis=1)
+    # convert category values to just numbers 0 or 1
+    for column in categories:
+        categories[column] = categories[column].str[-1]
+        categories[column] = categories[column].astype(int)
 
-# drop duplicates
-df.drop_duplicates(inplace=True)
+    # drop the original categories column from `df`
+    df.drop("categories", axis=1, inplace=True)
 
-# save the clean dataset into an sqlite database
-engine = create_engine('sqlite:///mydatabase.db')
-df.to_sql('mytable', engine, index=False)
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df, categories], axis=1)
+
+    # drop duplicates
+    df.drop_duplicates(inplace=True)
+
+    return df
+
+
+def save_data(df, database_filename):
+    """."""
+    # save the clean dataset into an sqlite database
+    engine = create_engine('sqlite:///mydatabase.db')
+    df.to_sql('mytable', engine, index=False)
+
+
+def main():
+    if len(sys.argv) == 4:
+
+        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
+              .format(messages_filepath, categories_filepath))
+        df = load_data(messages_filepath, categories_filepath)
+
+        print('Cleaning data...')
+        df = clean_data(df)
+
+        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+        save_data(df, database_filepath)
+
+        print('Cleaned data saved to database!')
+
+    else:
+        print('Please provide the filepaths of the messages and categories ' \
+              'datasets as the first and second argument respectively, as ' \
+              'well as the filepath of the database to save the cleaned data ' \
+              'to as the third argument. \n\nExample: python process_data.py ' \
+              'disaster_messages.csv disaster_categories.csv ' \
+              'DisasterResponse.db')
+
+
+if __name__ == '__main__':
+    main()
